c117a9bf6158f5404bd95deeba1687fd66edd8d6
ZagorodnyayaK <102464883+ZagorodnyayaK@users.noreply.github.com>
2024-01-19 15:19:19 +0200

    init

--- src/components/menu/menu.js
+++ src/components/menu/menu.js
@@ -21,42 +21,57 @@ const menuItems = [
   },
 ];
 
+// export default () => {
+//   const wrapper = document.querySelector('[data-module="menu"]');
+//   const docFragment = new DocumentFragment();
+//   const activeClassName = 'menu--item--active';
+//
+//   for (const {text, bgColor} of menuItems) {
+//     const link = document.createElement('a');
+//     link.classList.add('menu--item');
+//     link.style.backgroundColor = bgColor;
+//     link.innerHTML = text;
+//     link.href = '#';
+//     docFragment.appendChild(link);
+//   }
+//
+//   wrapper.innerHTML = '';
+//
+//   wrapper.appendChild(docFragment);
+//
+//   const onClickHandler = (e) => {
+//     e.preventDefault();
+//
+//     const nextLink = e.target;
+//     const activeLink = wrapper.querySelector('.' + activeClassName);
+//
+//     if (activeLink) {
+//       activeLink.classList.remove(activeClassName);
+//     }
+//
+//     if (activeLink !== nextLink) {
+//       nextLink.classList.add(activeClassName);
+//     }
+//   }
+//
+//   wrapper.addEventListener('click', onClickHandler)
+// };
+
 export default () => {
   const wrapper = document.querySelector('[data-module="menu"]');
   const docFragment = new DocumentFragment();
-  const activeClassName = 'menu--item--active';
 
-  for (const {text, bgColor} of menuItems) {
+  for (const item of menuItems) {
+
     const link = document.createElement('a');
+    const {text, bgColor} = item;
+
     link.classList.add('menu--item');
     link.style.backgroundColor = bgColor;
     link.innerHTML = text;
     link.href = '#';
+
     docFragment.appendChild(link);
   }
-
-  wrapper.innerHTML = '';
-
   wrapper.appendChild(docFragment);
-
-  const onClickHandler = (e) => {
-    e.preventDefault();
-
-    const nextLink = e.target;
-    const activeLink = wrapper.querySelector('.' + activeClassName);
-
-    if (activeLink) {
-      activeLink.classList.remove(activeClassName);
-    }
-
-    if (activeLink !== nextLink) {
-      nextLink.classList.add(activeClassName);
-    }
-  }
-
-  wrapper.addEventListener('click', onClickHandler)
-};
-
-
-
-
+}
